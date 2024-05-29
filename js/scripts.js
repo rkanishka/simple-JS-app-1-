c5899fd2f6efe557eba29a35c867c21f4a0f6800
rkanishka <kanishkaraghuvanshi97@gmail.com>
2024-05-29 12:44:55 -0700

    Add pokemon data

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,30 @@
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
-let favFood = " BIRYANI";
-document.write(favFood);
+let pokemonList=[
+  Name:"BABLASUR",
+  Height:7M,
+  Types:["GRASS","POISON"],
+  
+  Name:"PIKACHU",
+  Height:0.4M,
+  Types:"ELECTRIC",
+  
+  Name:"ZUBAT",
+  Height:0.8M,
+  Types:["POISON","FLYING"],
+  
+  Name:"MEOWTH",
+  Height:0.4M,
+  Types:"NORMAL",
+  
+  Name:"SEEL",
+  Height:1.1M,
+  Types:"WATER",
+  
+  Name:"DODUO",
+  Height:1.4M,
+  Types:["FLYIN","NORMAL"]
+  
+  ];
+document.write(pokemonList.Name);
+  
+
+
