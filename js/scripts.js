3a27fee2bd928c678e906c9fe70e81e4b3da428f
rkanishka <kanishkaraghuvanshi97@gmail.com>
2024-05-30 11:17:34 -0700

    IF-ELSE CONDITION DISPLAY

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,45 @@
-let pokemonList=[
+ let pokemonList=[
   {
-  Name:"BABLASUR",
-  Height:7,
-  Types:["GRASS","POISON"]
+    Name   : "BABLASUR",
+    Height : 7,
+    Types  : ["GRASS","POISON"]
   },
   {
-  Name:"PIKACHU",
-  Height:0.4,
-  Types:["ELECTRIC"]
+    Name   : "PIKACHU",
+    Height : 0.4,
+    Types  : ["ELECTRIC"]
   },
   {
-  Name:"ZUBAT",
-  Height:0.8,
-  Types:["POISON","FLYING"]
+    Name   : "ZUBAT",
+    Height : 0.8,
+    Types  : ["POISON","FLYING"]
   },
   {
-
-  Name:"MEOWTH",
-  Height:0.4,
-  Types:["NORMAL"]
+    Name   : "MEOWTH",
+    Height : 0.4,
+    Types  : ["NORMAL"]
   },
   {
-  Name:"SEEL",
-  Height:1.1,
-  Types:["WATER"]
+    Name   : "SEEL",
+    Height : 1.1,
+    Types  : ["WATER"]
   },
   {
-  Name:"DODUO",
-  Height:1.4,
-  Types:["FLYIN","NORMAL"]
+    Name   : "DODUO",
+    Height : 1.4,
+    Types  : ["FLYIN","NORMAL"]
  } 
   ];
-   for(let i=0 ; i<pokemonList.length ; i++)
-      {  
-        if (pokemonList[i].Height>1.0)
-          {
-            document.write(pokemonList[i].Name + "<br>"  + " " + pokemonList[i].Height + "(WOW THAT'S BIG)" + "<br>"  + " " + pokemonList[i].Types + "<br>");}
-           } 
+
+      for(let i=0; i<pokemonList.length ; i++)
+       {  
+         if (pokemonList[i].Height > 1.0)
+           {
+             document.write("Name:" + pokemonList[i].Name + "<br>"  + " " + "Height:" + pokemonList[i].Height + "(WOW THAT'S BIG)" + "<br>"  + " " + "Types:" +  pokemonList[i].Types + "<br><br>")
+           }
+         else
+           {
+             document.write("Name:" + pokemonList[i].Name + "<br>"  + " " + "Height:" + pokemonList[i].Height  + "<br>"  + " " + "Types:" +  pokemonList[i].Types + "<br><br>")
+           }
+       
+      } 
